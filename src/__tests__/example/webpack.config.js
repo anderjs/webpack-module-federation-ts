b2cf95278eaf7bc4013741d9cf31c2e8ac9ef1a5
Anderson Arevalo <aarevalo@nectia.com>
2024-01-01 22:57:16 -0500

    Plugin preload

---
+++ src/__tests__/example/webpack.config.js
@@ -0,0 +1,22 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+/**
+ * @type {import ('webpack').Configuration}
+ */
+const webpackConfig = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "app.bundle.js",
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    compress: true,
+    port: 9200,
+  },
+};
+
+module.exports = webpackConfig;
